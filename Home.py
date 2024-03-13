0877a9c703c4f0b08bcf413641c843fbd22c6a13
juinsherntong <118254001+juinsherntong@users.noreply.github.com>
2024-03-13 13:57:32 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,13 +1,13 @@
 import streamlit as st
 
-### Import images
-sb_title = Image.open('Sidebar title.png')
-mn_title = Image.open('Main title.png')
-legend = Image.open('Legend.png')
-
 st.set_page_config(
   page_title = 'Multipages App'
 )
 
 st.title('Home page')
 st.sidebar.success('Select a page below')
+
+### Import images
+sb_title = Image.open('Sidebar title.png')
+mn_title = Image.open('Main title.png')
+legend = Image.open('Legend.png')
