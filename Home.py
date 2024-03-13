3fefce8d99ac69fe195a8a1bf2f52df6c692b087
juinsherntong <118254001+juinsherntong@users.noreply.github.com>
2024-03-13 14:19:32 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,4 +1,9 @@
 import streamlit as st
+import streamlit.components.v1 as components
+import pandas as pd
+import networkx as nx
+from pyvis.network import Network
+from PIL import Image
 
 st.set_page_config(
   page_title = 'Multipages App'
