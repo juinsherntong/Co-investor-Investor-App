f7d1933fc3cd56439513a4e884ecb3b6ba4007d2
juinsherntong <118254001+juinsherntong@users.noreply.github.com>
2024-03-15 12:04:29 +0800

    Update 👤_Co-investor & Investor Database.py

--- pages/👤_Co-investor & Investor Database.py
+++ pages/👤_Co-investor & Investor Database.py
@@ -62,7 +62,7 @@ db_data = pd.read_csv('Data/Co-investor & Investor Database.csv')
 
 # Side bar (filters, dropdowns, etc)
 inv_categories_drp = list(db_data['Investor Categories'].drop_duplicates().sort_values())
-# inv_categories_drp.insert(0, 'All')
+inv_categories_drp.insert(0, 'All')
 # inv_categories_drp.remove('Investor')
 
 # with st.sidebar.expand('Filters for Investors'):
