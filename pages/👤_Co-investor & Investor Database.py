394da8a19df7bbd8281d86ce7fc8a6232f7639fa
juinsherntong <118254001+juinsherntong@users.noreply.github.com>
2024-03-15 12:11:12 +0800

    Update 👤_Co-investor & Investor Database.py

--- pages/👤_Co-investor & Investor Database.py
+++ pages/👤_Co-investor & Investor Database.py
@@ -67,10 +67,10 @@ inv_categories_drp = [item for item in inv_categories_drp if item != '']
 # inv_categories_drp = inv_categories_drp[0:-2]
 
 with st.sidebar.expand('Filters for Investors'):
-    inv_categories = st.multiselect('Investor categories',
-                                    inv_categories_drp,
-                                    'All',
-                                    help='Investor categories...')
+    # inv_categories = st.multiselect('Investor categories',
+    #                                 inv_categories_drp,
+    #                                 'All',
+    #                                 help='Investor categories...')
 
 # Display the dataframe as output
 st.dataframe(db_data)
