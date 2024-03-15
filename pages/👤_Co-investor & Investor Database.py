54dc5de96b65692089f2efc4977980e08ce5bd23
juinsherntong <118254001+juinsherntong@users.noreply.github.com>
2024-03-15 16:04:57 +0800

    Update 👤_Co-investor & Investor Database.py

--- pages/👤_Co-investor & Investor Database.py
+++ pages/👤_Co-investor & Investor Database.py
@@ -73,10 +73,13 @@ inv_types_drp = [item for item in inv_types_drp if item != '']
 HQ_country_drp = list(db_data['HQ Country/Territory'].drop_duplicates().sort_values())
 HQ_country_drp.insert(0, 'All')
 HQ_country_drp = [item for item in HQ_country_drp if item != '']
+HQ_country_drp = [item for item in HQ_country_drp if pd.notna(item)]
 
 HQ_region_drp = list(db_data['HQ Global Region'].drop_duplicates().sort_values())
 HQ_region_drp.insert(0, 'All')
 HQ_region_drp = [item for item in HQ_region_drp if item != '']
+HQ_region_drp = [item for item in HQ_region_drp if pd.notna(item)]
+
 
 with st.sidebar.expander('Filters for Investors'):
     inv_categories = st.multiselect('Investor categories',
