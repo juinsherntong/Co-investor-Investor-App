cd22f3478659302f9604eeb32fe978e1cbb56e4e
juinsherntong <118254001+juinsherntong@users.noreply.github.com>
2024-03-18 16:38:42 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -31,8 +31,5 @@ st.markdown(hide_img_fs, unsafe_allow_html=True)
 # Image in main panel (Co-investor / Investor DB)
 st.image(main_title, width=700)
 
-from streamlit_extras.app_logo import add_logo
-add_logo(st.sidebar.image(sidebar_title, width=300))
-
 # Image in sidebar (500 Data and Insights)
 # st.sidebar.image(sidebar_title, width=300)
