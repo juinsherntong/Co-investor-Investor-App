461091b10c66d3d4e21bbc43ff3daba1f65f6cab
juinsherntong <118254001+juinsherntong@users.noreply.github.com>
2024-03-18 15:14:24 +0800

    Update 👤_Co-investor & Investor Database.py

--- pages/👤_Co-investor & Investor Database.py
+++ pages/👤_Co-investor & Investor Database.py
@@ -55,6 +55,7 @@ st.markdown(hide_img_fs, unsafe_allow_html=True)
 st.image(main_title, width=700)
 
 # Image in sidebar (500 Data and Insights)
+st.sidebar.multipage_menu(my_caption)
 st.sidebar.image(sidebar_title, width=300)
 
 # Read CSV file (Co-investor & Investor Database.csv)
