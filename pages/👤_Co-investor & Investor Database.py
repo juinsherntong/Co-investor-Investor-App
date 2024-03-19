e6aafd9e3316a0ee053bc7caef1e7e63ed2a24ab
juinsherntong <118254001+juinsherntong@users.noreply.github.com>
2024-03-19 12:03:42 +0800

    Update 👤_Co-investor & Investor Database.py

--- pages/👤_Co-investor & Investor Database.py
+++ pages/👤_Co-investor & Investor Database.py
@@ -62,31 +62,31 @@ HQ_subregion_drp.insert(0, 'All')
 HQ_subregion_drp = [item for item in HQ_subregion_drp if item != '']
 HQ_subregion_drp = [item for item in HQ_subregion_drp if pd.notna(item)]
 
-with st.sidebar.expander('Filters for Investors' Demographic'):
-    inv_categories = st.multiselect('Select the investor category',
+with st.sidebar.expander("Filters for Investors' Demographic"):
+    inv_categories = st.multiselect("Select the investor category",
                                     inv_categories_drp,
                                     'All',
                                     help='For detailed definitions of each category, please refer to the Glossary section in the main section.')
     
-    inv_types = st.multiselect('Investor type(s)',
+    inv_types = st.multiselect("Investor type(s)",
                                     inv_types_drp,
                                     'All',
                                     help='Investor Types include detailed options within the broad categories of Angels/Incubators, VC, PE, Strategic Acquirers, and Others (Source: PitchBook)')
     
-    HQ_country = st.multiselect('Investors' Headquarters Country/Territory',
+    HQ_country = st.multiselect("Investors' Headquarters Country/Territory",
                                     HQ_country_drp,
                                     'All',
-                                    help='Investors' Headquarters Country/Territory denotes the geographical location where the investor's main headquarters is situated (Sourced: PitchBook).')
+                                    help="Investors' Headquarters Country/Territory denotes the geographical location where the investor's main headquarters is situated (Sourced: PitchBook).")
                                     
-    HQ_region = st.multiselect('Investors' Headquarters Regions',
+    HQ_region = st.multiselect("Investors' Headquarters Regions",
                                     HQ_region_drp,
                                     'All',
-                                    help='Investors' Headquarters Regions indicates the geographic area where the investor's primary headquarters is located (Sourced: PitchBook).')
+                                    help="Investors' Headquarters Regions indicates the geographic area where the investor's primary headquarters is located (Sourced: PitchBook).")
                                     
-    HQ_subregion = st.multiselect('Investors' Headquarters Sub-regions',
+    HQ_subregion = st.multiselect("Investors' Headquarters Sub-regions",
                                     HQ_subregion_drp,
                                     'All',
-                                    help='Investors' Headquarters Sub-regions refers to the specific geographic area where the investor's main headquarters is situated (Sourced: PitchBook).')
+                                    help="Investors' Headquarters Sub-regions refers to the specific geographic area where the investor's main headquarters is situated (Sourced: PitchBook).")
 
 # Side bar - investment preferences
 pref_industry_drp = list(db_data['Preferred Industry'].str.split(', ').explode('Preferred Industry').drop_duplicates().sort_values())
@@ -104,18 +104,18 @@ pref_inv_type_drp.insert(0, 'All')
 pref_inv_type_drp = [item for item in pref_inv_type_drp if item != '']
 pref_inv_type_drp = [item for item in pref_inv_type_drp if pd.notna(item)]
 
-with st.sidebar.expander('Filters for Investors' Investment Preference'):
-    pref_industries = st.multiselect('Investors' industry preference(s)',
+with st.sidebar.expander("Filters for Investors' Investment Preference"):
+    pref_industries = st.multiselect("Investors' industry preference(s)",
                                     pref_industry_drp,
                                     'All',
                                     help='These industries follow PitchBook's taxonomy and not 500's (Source: PitchBook)')
                                     
-    pref_verticals = st.multiselect('Investors' vertical preference(s)',
+    pref_verticals = st.multiselect("Investors' vertical preference(s)",
                                     pref_vertical_drp,
                                     'All',
                                     help='PitchBook verticals as defined here <https://pitchbook.com/what-are-industry-verticals>. Note that 500's portfolio company verticals (as seen in Salesforce) are more expansive. (Source: PitchBook)')
                                     
-    pref_inv_types = st.multiselect('Investors' preferred deal type(s)',
+    pref_inv_types = st.multiselect("Investors' preferred deal type(s)",
                                     pref_inv_type_drp,
                                     'All',
                                     help='(Source: PitchBook)')
